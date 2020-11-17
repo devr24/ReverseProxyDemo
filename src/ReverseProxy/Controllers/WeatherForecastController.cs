9c31f80441183f00709e7cc03ad45bb6a3dc0436
Rob <rmccabe@eshopworld.com>
2020-11-17 00:16:40 +0000

    Added Swagger parser

--- src/ReverseProxy/Controllers/WeatherForecastController.cs
+++ src/ReverseProxy/Controllers/WeatherForecastController.cs
@@ -5,6 +5,11 @@ using ReverseProxy.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.OpenApi.Extensions;
+using Microsoft.OpenApi.Readers;
+using Microsoft.OpenApi;
 
 namespace ReverseProxy.Controllers
 {
@@ -31,12 +36,33 @@ namespace ReverseProxy.Controllers
         {
             var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+                {
+                    Date = DateTime.Now.AddDays(index),
+                    TemperatureC = rng.Next(-20, 55),
+                    Summary = Summaries[rng.Next(Summaries.Length)]
+                })
+                .ToArray();
+        }
+    }
+
+    [ApiController]
+    [Route("[controller]")]
+    public class SwaggerReaderController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult> Get(string uri)
+        {
+            using var httpClient = new HttpClient();
+
+            var str = await httpClient.GetStringAsync(uri);
+
+            // Read V3 as YAML
+            var openApiDocument = new OpenApiStringReader().Read(str, out var diagnostic); // new OpenApiStreamReader().Read(stream, out var diagnostic);
+
+            // Write V2 as JSON
+            var outputString = openApiDocument.Serialize(OpenApiSpecVersion.OpenApi2_0, OpenApiFormat.Json);
+
+            return Ok(openApiDocument);
         }
     }
 }
